34792d15b6bc517bfb8123bc1b8c90e5daad6058
Reinaert Van de Cruys <reinaert.vandecruys@gmail.com>
2015-12-05 17:33:26 +0100

    Implement erasing of drawings.

--- PhotoTouch/PhotoFrame.xaml.cs
+++ PhotoTouch/PhotoFrame.xaml.cs
@@ -23,56 +23,110 @@ namespace PhotoTouch
     /// </summary>
     public partial class PhotoFrame : UserControl
     {
+        // current mode (true = draw mode, false = move mode)
+        bool mode = true;
+
+        // current drawing mode (true = pencil, false = erase)
+        bool drawingMode = true;
+
+        // current drawing color
+        Color drawingColor;
+
+        // colored circle in the draw button
+        Ellipse buttonDrawContent = new Ellipse();
+
+        // actual frame of this photo frame control
+        ScatterViewItem frame;
+
+        // drawing canvas in this photo frame
+        SurfaceInkCanvas canvas;
+
+        // buttons at the bottom of this photo frame
+        SurfaceButton buttonMode;
+        SurfaceButton buttonDraw;
+        SurfaceButton buttonClose;
+
         public PhotoFrame()
         {
             InitializeComponent();
 
-            SwitchMode(this, null);
+            // get the different controls of this photo frame
+            frame = (ScatterViewItem)this.FindName("Frame");
+            canvas = (SurfaceInkCanvas)this.FindName("Canvas");
+            buttonMode = (SurfaceButton)this.FindName("ButtonMode");
+            buttonDraw = (SurfaceButton)this.FindName("ButtonDraw");
+            buttonClose = (SurfaceButton)this.FindName("ButtonDraw");
+
+            // initialize the mode of the photo frame
+            SetMode();
+
+            // initialize the colored circle for the draw button
+            buttonDrawContent.Width = 38;
+            buttonDrawContent.Height = 38;
+
+            // create the icon in the close button
             ((SurfaceButton)FindName("ButtonClose")).Content = FindResource("CloseIcon");
+
+            // set the default drawing color
+            SetDrawingColor(Color.FromRgb(255, 0, 0));
         }
 
-
-
-
-
-
-
-
-
-
-        void SwitchMode(object sender, RoutedEventArgs e)
+        void SetMode()
         {
-            // get the actual photo frame
-            ScatterViewItem frame = (ScatterViewItem)this.FindName("Frame");
-            
-            // get the drawing canvas of the photo frame
-            SurfaceInkCanvas canvas = (SurfaceInkCanvas)this.FindName("Canvas");
-
-            // get the mode butten and draw button of the photo drame
-            SurfaceButton buttonMode = (SurfaceButton)this.FindName("ButtonMode");
-            SurfaceButton buttonDraw = (SurfaceButton)this.FindName("ButtonDraw");
-
-            // if the canvas is drawable, we are in draw mode, so switch to move mode
-            if (canvas.IsHitTestVisible)
+            SetDrawingMode();
+            if (mode)
             {
                 canvas.IsHitTestVisible = false;
-                buttonMode.Content = FindResource("DrawIcon");
+                buttonMode.Content = FindResource("MoveIcon");
                 buttonDraw.Content = null;
             }
-            // otherwise, we are in move mode, so switch to draw mode
             else
             {
                 canvas.IsHitTestVisible = true;
-                buttonMode.Content = FindResource("MoveIcon");
-                buttonDraw.Content = FindResource("PencilIcon");
+                buttonMode.Content = FindResource("DrawIcon");
             }
+        }
+        
+        void SwitchMode(object sender, RoutedEventArgs e)
+        {
+            mode = !mode;
+            SetMode();
+        }
+
+        void SetDrawingMode()
+        {
+            if (drawingMode)
+            {
+                buttonDraw.Content = buttonDrawContent;
+                canvas.EditingMode = SurfaceInkEditingMode.Ink;
+            }
+            else
+            {
+                buttonDraw.Content = FindResource("EraseIcon"); ;
+                canvas.EditingMode = SurfaceInkEditingMode.EraseByPoint;
+            }
+        }
+
+        void SwitchDrawingMode(object sender, RoutedEventArgs e)
+        {
+            if (mode)
+            {
+                return;
+            }
+            drawingMode = !drawingMode;
+            SetDrawingMode();
+        }
+
+        void SetDrawingColor(Color color)
+        {
+            drawingColor = color;
+            buttonDrawContent.Fill = new SolidColorBrush(drawingColor);
+            ((SurfaceInkCanvas)FindName("Canvas")).DefaultDrawingAttributes.Color = drawingColor;
         }
 
         void Close(object sender, RoutedEventArgs e)
         {
-            // get the actual photo frame
-            ScatterViewItem frame = (ScatterViewItem)this.FindName("Frame");
-
+            // remove the photo frame from the table
             ((ScatterView)frame.Parent).Items.Remove(frame);
         }
     }
